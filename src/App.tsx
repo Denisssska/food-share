6c283c41506217212806d7bf811256374788a051
Denisssska <yarmoshkoden18m@gmail.com>
2022-12-23 06:05:37 +0300

    add aboutUs ContactUs

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ function App() {
             <CardBody>
                 <Routes>
                     <Route path={"/"} element={<Main/>}/>
+                    <Route path={"/*"} element={<Main/>}/>
                     <Route path={"/login"} element={<Login/>}/>
                     <Route path={"/registration"} element={<Registration/>}/>
                     <Route path={"/oneProd"} element={<ProductPage/>}/>
